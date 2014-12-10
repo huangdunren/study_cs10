0ba96c28afd18f9c04f3940afa8d51b4f53801cd
黄敦仁 <hdr@kouatsuhitos-Mac-mini.local>
2014-12-10 17:18:41 +0800

    Initial Commit

---
+++ cs10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cs10
+//
+//  Created by 黄敦仁 on 14/12/10.
+//  Copyright (c) 2014年 hdr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
